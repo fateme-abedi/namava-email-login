be0f32cd10fad70ea7c520927020a7e4f24c4382
fatemeabedi <abedifateme1378@gmail.com>
2023-10-23 20:03:10 +0330

    create index

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
-import Test from "./components/Test/index.jsx";
-import Test2 from "./components/Test2/index.jsx";
+import Header from "./components/Header/index.js";
+import Login from "./components/Login";
 
 const App = () => {
   return (
-    <>
-      <div>App</div>
-      <img src="./assets/cr7.jpeg" alt="cr7" />
-      <Test />
-      <Test2 />
-    </>
+    <div>
+      <Header />
+      <Login />
+    </div>
   );
 };
 
