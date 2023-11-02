a8cab0c6b15345dd0520e39b48dc94f66fb4ac31
fatemeabedi <abedifateme1378@gmail.com>
2023-11-02 16:36:23 +0330

    create commnet component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/index.js";
+import Header from "./components/CommentComponents/Header/index.js";
 import Login from "./components/Login";
+import CommentPage from "./pages/CommentPage";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Login />
+      <CommentPage />
     </div>
   );
 };
